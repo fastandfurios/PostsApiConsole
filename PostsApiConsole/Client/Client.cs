c21aab11cadbc9a0aa492325564e25d0f62582f6
Дмитрий <semu-dima@yandex.ru>
2021-07-30 10:03:31 +0300

    :tada: added a client

---
+++ PostsApiConsole/Client/Client.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace PostsApiConsole.Client
+{
+    public class Client : IClient<Response, int>
+    {
+        internal HttpClient HttpClient { get; private set; }
+
+        public Client(HttpClient httpClient)
+        {
+            HttpClient = httpClient;
+        }
+
+        public async Task<Response> GetPosts(int id)
+        {
+            var httpRequest = new HttpRequestMessage(HttpMethod.Get,
+                $"https://jsonplaceholder.typicode.com/posts/{id}");
+
+            try
+            {
+                var response = await HttpClient.SendAsync(httpRequest);
+
+                await using var responseStream = await response.Content.ReadAsStreamAsync();
+
+                var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+
+                return await JsonSerializer.DeserializeAsync<Response>(responseStream, options);
+            }
+            catch(AggregateException e)
+            {
+                Debug.Write(e.Message);
+            }
+
+            return default;
+        }
+    }
+}
--- PostsApiConsole/Program.cs
+++ PostsApiConsole/Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace PostsApiConsole
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            
+            var httpClient = new HttpClient();
+            var client = new Client.Client(httpClient);
+            var result = await client.GetPosts(4);
+            Console.WriteLine($"{result.UserId}\n{result.Id}\n{result.Title}\n{result.Body}");
         }
     }
 }
